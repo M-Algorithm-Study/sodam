49470505fa3cf83d994996a823b9f67984c2d2c1
sodamjeong <sodam828@naver.com>
2023-08-01 14:14:11 +0900

    add 5week common

---
+++ 5week/암호만들기.py
@@ -0,0 +1,14 @@
+# 백준 / 1759번 암호만들기
+
+from itertools import combinations
+n,m = map(int,input().split())
+lst = sorted(input().split())
+
+for i in combinations(lst,n):
+    cnt = 0
+    for j in i:
+        if j in 'aeiou':
+            cnt += 1
+
+    if cnt >= 1 and cnt <= n - 2:
+        print(''.join(i))
