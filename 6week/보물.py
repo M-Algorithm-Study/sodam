a924dc2e2fed74a7c3981ec460d68096ea0ae721
sodamjeong <sodam828@naver.com>
2023-08-08 09:24:58 +0900

    ADD 6week personal

---
+++ 6week/보물.py
@@ -0,0 +1,10 @@
+# 백준 / 1026번 보물
+
+n = int(input())
+a = sorted(list(map(int,input().split())))
+b = sorted(list(map(int,input().split())))
+cnt = 0
+
+for i in range(n):
+    cnt += (a[i] * b[-i-1])
+print(cnt)
