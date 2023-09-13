419ee7e3e055d148c6571e790d6eb456b2f2c881
sodamjeong <sodam828@naver.com>
2023-09-13 17:13:14 +0900

    ADD 12week

---
+++ 12week/트리순회.py
@@ -0,0 +1,35 @@
+# 백준 / 1991번 트리순회
+
+import sys
+input = sys.stdin.readline
+
+tree = {}
+
+for _ in range(int(input())):
+    root, left, right = map(str,input().split())
+    tree[root] = [left, right]
+  
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+
