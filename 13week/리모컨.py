1ef56b68bfd0bde177e37860bcfefc4c6feac334
sodamjeong <sodam828@naver.com>
2023-09-25 16:08:04 +0900

    update;

--- 13week/리모컨.py
+++ 13week/리모컨.py
@@ -3,27 +3,26 @@
 import sys
 input = sys.stdin.readline
 
-n=int(input())
-m=int(input())
-lst=list(input().strip())
-btn=abs(n-100)
+n,m = int(input()), int(input())
+lst = list(input().strip())
+btn = abs(n-100)
 
 if m == 0:
-  print(min(btn,len(str(n)))) 
+  print(min(btn, len(str(n)))) 
 # 고장난 버튼이 0개일 경우 버튼이 눌리는 최솟값 출력
 else: 
 # 완전탐색 (주어지는 제일 큰 채널 500,000 기준 최댓값 999,901까지 탐색)
   for i in range(999901):
-    i=str(i)
-    chk=1
+    i = str(i)
+    chk = 1
     # 채널 숫자 중 고장난 버튼이 있으면 false
     for j in i:
       if j in lst:
-        chk=0
+        chk = 0
         break
         
     if chk: #고장난 버튼이 없으면 최솟값 재설정
-      btn=min(btn, abs(n-int(i))+len(i))
+      btn = min(btn, abs(n-int(i))+len(i))
   print(btn)
 
 
--- 13week/헌내기.py
+++ 13week/헌내기.py
@@ -13,25 +13,25 @@ cnt = 0
 
 # 시작점인 도연이를 찾아 que 에 담아주고 시작
 for x in range(n):
-    for y in range(m):
-        if campus[x][y] == 'I':
-            que.append((x,y))
-            visit[x][y] = 1
+  for y in range(m):
+    if campus[x][y] == 'I':
+      que.append((x,y))
+      visit[x][y] = 1
 
 # bfs로 갈 수 있는 영역에서 만나는 p(사람) 수를 확인하여 +1
 while que:
-    x, y = que.popleft()
-    for d in dir:
-        dx, dy = x+d[0], y+d[1]
-        
-        if 0<=dx<n and 0<=dy<m:
-            # 범위 내 벽도 아니고 방문을 안한 경우
-            if campus[dx][dy] != 'X' and visit[dx][dy] != 1:
-                que.append((dx,dy))
-                visit[dx][dy] = 1
-                # 근데 그게 사람일 경우 cnt +1
-                if campus[dx][dy] == 'P':
-                    cnt += 1
+  x, y = que.popleft()
+  for d in dir:
+    dx, dy = x+d[0], y+d[1]
+    
+    if 0<=dx<n and 0<=dy<m:
+      # 범위 내 벽도 아니고 방문을 안한 경우
+      if campus[dx][dy] != 'X' and visit[dx][dy] != 1:
+        que.append((dx,dy))
+        visit[dx][dy] = 1
+        # 근데 그게 사람일 경우 cnt +1
+        if campus[dx][dy] == 'P':
+          cnt += 1
 
 # 만날 수 있는 P 의 수가 0 이면 TT 아니면 cnt 출력                    
 print(cnt if cnt > 0 else 'TT')
