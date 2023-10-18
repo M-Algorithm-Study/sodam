95dc717e1e241eda3be1445c305eb5f888a3e6e1
sodamjeong <sodam828@naver.com>
2023-10-18 17:49:38 +0900

    ADD 16week

---
+++ 16week/2n타일링.py
@@ -0,0 +1,14 @@
+# 백준 / 11727번 2×n 타일링 2
+
+n = int(input())
+dp = [0] * (n+1)
+
+dp[1] = 1
+
+if n > 1 :
+    dp[2] = 3
+
+for i in range(3, n+1) :
+    dp[i] = dp[i-2] * 2 + dp[i-1]
+
+print(dp[n] % 10007)
