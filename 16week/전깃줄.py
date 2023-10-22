afd84b750d49195b57b82deb9f8bee8898b2ab5e
sodamjeong <sodam828@naver.com>
2023-10-22 11:43:49 +0900

    update

--- 16week/전깃줄.py
+++ 16week/전깃줄.py
@@ -7,6 +7,7 @@ n = int(input())
 ans = [1]*n
 line = sorted([list(map(int,input().split())) for _ in range(n)])
 
+# 연결 가능한 전깃줄 수 확인 (오름차순으로 정렬하여 오른쪽 수가 큰 전깃줄) 
 for i in range(n):
     for j in range(i):
         if line[i][1] > line[j][1]:
