1c9d154469c0184640751b5c1ee070c449d9cb69
sodamjeong <sodam828@naver.com>
2023-10-29 16:33:48 +0900

    common add

--- 17week/파이프옮기기.py
+++ 17week/파이프옮기기.py
@@ -5,18 +5,23 @@ input  = sys.stdin.readline
 
 n = int(input())
 home = [list(map(int,input().split())) for _ in range(n)]
-dp = [[[0,0,0] for _ in range(n)] for _ in range(n+1)]
-dp[1][1] = [1,0,0]
+dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(3)]
 
-for i in range(1,n+1):
+dp[0][0][1] = 1
+
+for i in range(2,n):
+    if home[0][i] == 0:
+        dp[0][0][i] = dp[0][0][i-1]
+
+for i in range(1,n):
     for j in range(1,n):
-        if i==j==1:
-            continue
+        if home[i][j] == 0 and home[i-1][j] == 0 and home[i][j-1] == 0:
+            dp[1][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1]
+        
         if home[i][j] == 0:
-            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1]
-            dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2]
-            if home[i][j-1] == home[i-1][j] == 0 :
-                dp[i][j][1] = sum(dp[i-1][j-1])
+            dp[0][i][j] = dp[0][i][j-1]+ dp[1][i][j-1]
+            dp[2][i][j] = dp[1][i-1][j] + dp[2][i-1][j]
 
-print(sum(dp[-1][-1]))
+print(sum(dp[i][n-1][n-1] for i in range(3)))
 
+# 이해가 조곰 덜 된 상태..... 더 공부 해보겠습니다..
